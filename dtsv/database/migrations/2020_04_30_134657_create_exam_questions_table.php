209b88f131967ecbbeb3f73b17074bf1845fa882
Hoanghai061425 <52031827+Hoanghai061425@users.noreply.github.com>
2020-05-01 21:22:58 +0700

    abc
    abc

---
+++ dtsv/database/migrations/2020_04_30_134657_create_exam_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_questions', function (Blueprint $table) {
+
+            $table->integer('id_exam')->unsigned();
+            $table->integer('id_question')->unsigned();
+
+            $table->foreign('id_exam')->references('id_exam')->on('exams')->onDelete('cascade');
+            $table->foreign('id_question')->references('id_question')->on('questions')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_questions');
+    }
+}
