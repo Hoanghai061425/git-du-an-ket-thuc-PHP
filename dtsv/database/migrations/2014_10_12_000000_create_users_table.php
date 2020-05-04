b69567878f745ae95b81f3002cec10dac8358500
Hoanghai061425 <52031827+Hoanghai061425@users.noreply.github.com>
2020-05-04 19:01:46 +0700

    up exam

--- dtsv/database/migrations/2014_10_12_000000_create_users_table.php
+++ dtsv/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
 
             $table->increments('id');
-            $table->string('maSv')->nullable();
+            $table->string('maSv')->nullable()->unique();
 
             $table->string('full_name');
            // $table->string('username')->unique();
